b1184c312d3963de5b324777f4a743be057535f1
liam4 <towerofnix@gmail.com>
2017-06-11 10:52:29 -0300

    Loop-play.js cleanup

--- src/loop-play.js
+++ src/loop-play.js
@@ -1,16 +1,95 @@
 'use strict'
 
-const fs = require('fs')
-const tempy = require('tempy')
-
 const { spawn } = require('child_process')
-const { promisify } = require('util')
-const fetch = require('node-fetch')
-const path = require('path')
 const promisifyProcess = require('./promisify-process')
 const sanitize = require('sanitize-filename')
+const tempy = require('tempy')
 
-const writeFile = promisify(fs.writeFile)
+class DownloadController {
+  constructor(picker, downloader) {
+    this.process = null
+
+    this.picker = picker
+    this.downloader = downloader
+  }
+
+  async downloadNext() {
+    const picked = this.picker()
+
+    if (picked == null) {
+      return false
+    }
+
+    const [ title, downloaderArg ] = picked
+    console.log(`Downloading ${title}..\nDownloader arg: ${downloaderArg}`)
+
+    const from = await this.downloader(downloaderArg)
+
+    const tempDir = tempy.directory()
+    const to = tempDir + `/.${sanitize(title)}.wav`
+
+    // We pass false to promisifyProcess to show we want hte output of avconv
+    // to be silenced.
+    const convertProcess = spawn('avconv', ['-y', '-i', from, to])
+    const convertPromise = promisifyProcess(convertProcess, false)
+
+    this.wavFile = to
+    this.process = convertProcess
+
+    try {
+      await convertPromise
+    } catch(err) {
+      console.warn("Failed to convert " + title)
+      console.warn("Selecting a new track\n")
+
+      this.killProcess()
+
+      return await this.downloadNext()
+    }
+  }
+
+  killProcess() {
+    if (this.process) {
+      this.process.kill()
+    }
+  }
+}
+
+class PlayController {
+  constructor(downloadController) {
+    this.playArgs = []
+    this.process = null
+
+    this.downloadController = downloadController
+  }
+
+  async loopPlay() {
+    await this.downloadController.downloadNext()
+
+    while (this.downloadController.wavFile) {
+      const nextPromise = this.downloadController.downloadNext()
+
+      const file = this.downloadController.wavFile
+      const playProcess = spawn('play', [...this.playArgs, file])
+      const playPromise = promisifyProcess(playProcess)
+      this.process = playProcess
+
+      try {
+        await playPromise
+      } catch(err) {
+        console.warn(err)
+      }
+
+      await nextPromise
+    }
+  }
+
+  killProcess() {
+    if (this.process) {
+      this.process.kill()
+    }
+  }
+}
 
 module.exports = function loopPlay(picker, downloader, playArgs = []) {
   // Looping play function. Takes one argument, the "pick" function,
@@ -20,79 +99,23 @@ module.exports = function loopPlay(picker, downloader, playArgs = []) {
   // function is null (or similar). Optionally takes a second argument
   // used as arguments to the `play` process (before the file name).
 
-  let playProcess, convertProcess
+  const downloadController = new DownloadController(picker, downloader)
 
-  async function downloadNext() {
-    const picked = picker()
+  const playController = new PlayController(downloadController)
+  playController.playArgs = playArgs
 
-    if (picked == null) {
-      return false
-    }
-
-    const [ title, downloaderArg ] = picked
-    console.log(`Downloading ${title}..\nDownloader arg: ${downloaderArg}`)
-
-    const downloadFile = await downloader(downloaderArg)
-
-    const tempDir = tempy.directory()
-    const wavFile = tempDir + `/.${sanitize(title)}.wav`
-
-    try {
-      const convertPromise = convert(downloadFile, wavFile)
-      convertProcess = convertPromise.process
-      await convertPromise
-    } catch(err) {
-      console.warn("Failed to convert " + title)
-      console.warn("Selecting a new track\n")
-
-      return await downloadNext()
-    }
-
-    return wavFile
-  }
-
-  async function main() {
-    let wavFile = await downloadNext()
-
-    while (wavFile) {
-      const nextPromise = downloadNext()
-
-      // What a mouthful!
-      const playPromise = playFile(wavFile, playArgs)
-      playProcess = playPromise.process
-
-      try {
-        await playPromise
-      } catch(err) {
-        console.warn(err)
-      }
-
-      wavFile = await nextPromise
-    }
-  }
-
-  const promise = main()
+  const promise = playController.loopPlay()
 
   return {
     promise,
 
     skip: function() {
-      if (playProcess) playProcess.kill()
+      playController.killProcess()
     },
 
     kill: function() {
-      if (playProcess) playProcess.kill()
-      if (convertProcess) convertProcess.kill()
+      playController.killProcess()
+      downloadController.killProcess()
     }
   }
-}
-
-function convert(fromFile, toFile) {
-  const avconv = spawn('avconv', ['-y', '-i', fromFile, toFile])
-  return promisifyProcess(avconv, false)
-}
-
-function playFile(file, opts = []) {
-  const play = spawn('play', [...opts, file])
-  return Object.assign(promisifyProcess(play), {process: play})
 }
